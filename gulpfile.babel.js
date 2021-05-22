ca0795bd2ce3a738ceb529604244afea06fc54af
John Denlinger <justjohnd@gmail.com>
2021-05-22 12:42:09 +0900

    Change images to img, add build and dev tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,4 @@
+import { src, dest, watch, series, parallel } from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
@@ -5,8 +6,8 @@ import gulpif from 'gulp-if';
 import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'autoprefixer';
-import { src, dest, watch } from 'gulp';
 import imagemin from 'gulp-imagemin';
+import del from 'del';
 const PRODUCTION = yargs.argv.prod;
 
 // Compile css
@@ -20,15 +21,33 @@ export const style = () => {
     .pipe(dest('dist/css'));
 };
 
-//Optimize Images
-export const images = () => {
+//Optimize img
+export const img = () => {
   return src('src/img/**/*.{jpg,jpeg,png,svg,gif}')
     .pipe(gulpif(PRODUCTION, imagemin()))
-    .pipe(dest('dist/images'));
+    .pipe(dest('dist/img'));
 };
+
+// Copy files from src not included in any directories to dist
+export const copy = () => {
+  return src([
+    'src/**/*',
+    '!src/{img,js,sass}',
+    '!src/{img,js,sass}/**/*',
+  ]).pipe(dest('dist'));
+};
+
+// Delete dist on build
+export const clean = () => del(['dist']);
+
+// Set dev and build tasks
+export const dev = series(clean, parallel(style, img, copy), watchAll);
+export const build = series(clean, parallel(style, img, copy));
+export default dev;
 
 //Watch for changes
 export const watchAll = () => {
   watch('src/sass/**/*.scss', style);
-  watch('src/img/**/*.{jpg,jpeg,png,svg,gif}', images);
+  watch('src/img/**/*.{jpg,jpeg,png,svg,gif}', img);
+  watch(['src/**/*', '!src/{img,js,sass}', '!src/{img,js,sass}/**/*'], copy);
 };
