b9ed883bbb8f169508a0de5b05d2a418608ff8a6
John Denlinger <justjohnd@gmail.com>
2021-05-26 10:06:39 +0900

    Change fontawesome to cdn to hotfix color issue

--- header.php
+++ header.php
@@ -18,6 +18,9 @@
 		charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css"
+		integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous"> -->
 
 	<!-- Multiimage carousel. Note: currently doesn't work without CDN-->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -26,7 +29,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
 	</script>
-	<?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
