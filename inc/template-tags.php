c0d36e66949234e1c3711e43f74ebb7a5ba7c092
John Denlinger <justjohnd@gmail.com>
2021-05-28 12:09:14 +0900

    Pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -76,7 +76,7 @@ if (!function_exists('bootstrap_theme_entry_footer')) :
 		}
 
 		if (!is_single() && !post_password_required() && (comments_open() || get_comments_number())) {
-			echo '<span class="comments-link">';
+			echo '<br><span class="comments-link">';
 			comments_popup_link(
 				sprintf(
 					wp_kses(
@@ -94,22 +94,22 @@ if (!function_exists('bootstrap_theme_entry_footer')) :
 			echo '</span>';
 		}
 
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__('Edit <span class="screen-reader-text">%s</span>', 'bootstrap-theme'),
-					[
-						'span' => [
-							'class' => [],
-						],
-					]
-				),
-				wp_kses_post(get_the_title())
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
+		// edit_post_link(
+		// 	sprintf(
+		// 		wp_kses(
+		// 			/* translators: %s: Name of current post. Only visible to screen readers */
+		// 			__('Edit <span class="screen-reader-text">%s</span>', 'bootstrap-theme'),
+		// 			[
+		// 				'span' => [
+		// 					'class' => [],
+		// 				],
+		// 			]
+		// 		),
+		// 		wp_kses_post(get_the_title())
+		// 	),
+		// 	'<span class="edit-link">',
+		// 	'</span>'
+		// );
 	}
 endif;
 
